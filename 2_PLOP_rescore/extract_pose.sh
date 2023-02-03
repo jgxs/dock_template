961efc882577a2bc638eec8ac8f1c49b8dfa1c24
Yujie Cheng <yjcheng@x254.hn.org>
2023-02-03 19:57:06 +0800

    reset some numbers

--- 2_PLOP_rescore/extract_pose.sh
+++ 2_PLOP_rescore/extract_pose.sh
@@ -12,7 +12,7 @@ date
 
 export DOCKBASE="/home/soft/ucsfdock/DOCK-3.7-bbe1a30c/"
 
-# ls dock_* -d > dirlist
+ls dock_* -d > dirlist
 /home/yjcheng/miniconda3/envs/dock37/bin/python $DOCKBASE/analysis/extract_all.py -d --done 
 echo "finish first "
 /home/yjcheng/miniconda3/envs/dock37/bin/python $DOCKBASE/analysis/getposes.py -l 50000
